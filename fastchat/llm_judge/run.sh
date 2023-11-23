ed40de49b84c8750a4449c27e22b3b0df3ae57fc
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2023-11-23 13:36:02 +0000

    Fix script

--- fastchat/llm_judge/run.sh
+++ fastchat/llm_judge/run.sh
@@ -3,7 +3,7 @@
 set -x -e
 HUB_MODEL_ID=$1
 MT_BENCH_ID=$2
-[ -z "$3" ] && DTYPR="float16" || DTYPE=$3
+[ -z "$3" ] && DTYPE="float16" || DTYPE=$3
 
 # Generate answer
 python gen_model_answer.py --model-path $HUB_MODEL_ID --model-id $MT_BENCH_ID --dtype $DTYPE
