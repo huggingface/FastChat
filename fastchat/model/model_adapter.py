d168119f54bbab7731019ec7eea520b142169e30
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2023-12-01 15:10:16 +0000

    Add DeepSeek adapter

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -1294,6 +1294,21 @@ class MistralAdapter(BaseModelAdapter):
     def get_default_conv_template(self, model_path: str) -> Conversation:
         return get_conv_template("h4_default_v3")
 
+class H4DeepSeekAdapter(BaseModelAdapter):
+    """The model adapter for H4 DeepSeek models"""
+
+    def match(self, model_path: str):
+        return "deepseek" in model_path.lower()
+
+    def load_model(self, model_path: str, from_pretrained_kwargs: dict):
+        model, tokenizer = super().load_model(model_path, from_pretrained_kwargs)
+        model.config.eos_token_id = tokenizer.eos_token_id
+        model.config.pad_token_id = tokenizer.pad_token_id
+        return model, tokenizer
+
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("h4_default_v3")
+
 
 class CuteGPTAdapter(BaseModelAdapter):
     """The model adapter for llama-2"""
@@ -1680,6 +1695,7 @@ register_model_adapter(OpenLLaMaOpenInstructAdapter)
 register_model_adapter(ReaLMAdapter)
 register_model_adapter(CodeLlamaAdapter)
 register_model_adapter(MistralAdapter)
+register_model_adapter(H4DeepSeekAdapter)
 
 # After all adapters, try the default base adapter.
 register_model_adapter(BaseModelAdapter)
