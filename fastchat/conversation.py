50a8120d8e5694bf4e1ecba23c6261b7c08d194d
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2024-02-29 16:21:00 +0000

    Fix Gemma again :(

--- fastchat/conversation.py
+++ fastchat/conversation.py
@@ -971,6 +971,17 @@ register_conv_template(
     )
 )
 
+# register_conv_template(
+#     Conversation(
+#         name="gemma",
+#         system_message="<bos>",
+#         roles=("<start_of_turn>user\n", "<start_of_turn>model\n"),
+#         sep_style=SeparatorStyle.NO_COLON_SINGLE,
+#         sep="<end_of_turn>\n",
+#         stop_str="<end_of_turn>",
+#     )
+# )
+
 if __name__ == "__main__":
     print("Vicuna template:")
     conv = get_conv_template("vicuna_v1.1")
